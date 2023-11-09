e7b8426e2399191f76b17e88c383d03206a2febb
Justin Falcone <justin@justinfalcone.com>
2023-11-09 13:32:12 -0500

    source info

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::{collections::HashMap, rc::Rc};
-
 /* Gremlin grammar
 Stmt =
     | "let" Binding (":" TyExpr)? ":=" Expr
@@ -32,217 +30,317 @@ Op = "+" | "-" | "*" | "/" | "&" | "@"
 Comment = "#" ... eol
 */
 
-#[derive(Clone, Debug, PartialEq, Eq)]
-struct Tok {
-    kind: TokKind,
-    // TODO: token info
-}
-
-impl Tok {
-    fn new(kind: TokKind) -> Self {
-        Self { kind }
-    }
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash)]
-enum TokKind {
-    // keywords
-    Let,
-    While,
-    Do,
-    End,
-    Return,
-    Func,
-    Struct,
-    If,
-    Then,
-    Else,
-    As,
-    True,
-    False,
-    // punctuation
-    Colon,
-    ColonEq,
-    ParLeft,
-    ParRight,
-    Arrow,
-    Semi,
-    AngleLeft,
-    AngleRight,
-    And,
-    Dot,
-    Comma,
-    Plus,
-    Minus,
-    Star,
-    Slash,
-    At,
-    Identifier(Identifier),
-    IntLiteral(IntLiteral),
-    // control
-    EndOfInput,
-}
-
-impl TokKind {
-    pub fn from_ident(str: String) -> TokKind {
-        KEYWORD_TOKENS
-            .with(|pair| pair.clone())
-            .0
-            .get(&str)
-            .cloned()
-            .unwrap_or(TokKind::Identifier(Identifier { value: str }))
+mod source_info {
+    #[derive(Copy, Clone, Debug, PartialEq, Eq)]
+    pub struct SourceInfo {
+        pub start: usize,
+        pub length: usize,
     }
 
-    fn to_keyword(&self) -> Option<String> {
-        KEYWORD_TOKENS
-            .with(|pair| pair.clone())
-            .1
-            .get(self)
-            .map(|s| s.to_string())
-    }
-}
-
-type KeywordTokens = Rc<(HashMap<String, TokKind>, HashMap<TokKind, String>)>;
-
-fn keyword_tokens() -> KeywordTokens {
-    let pairs = vec![
-        (TokKind::Let, "let"),
-        (TokKind::While, "while"),
-        (TokKind::Func, "func"),
-        (TokKind::Struct, "struct"),
-        (TokKind::Do, "do"),
-        (TokKind::Return, "return"),
-        (TokKind::If, "if"),
-        (TokKind::Then, "then"),
-        (TokKind::Else, "else"),
-        (TokKind::End, "end"),
-        (TokKind::True, "true"),
-        (TokKind::False, "false"),
-    ];
-
-    Rc::new((
-        pairs
-            .iter()
-            .map(|(token, str)| (str.to_string(), token.clone()))
-            .collect(),
-        pairs
-            .iter()
-            .map(|(token, str)| (token.clone(), str.to_string()))
-            .collect(),
-    ))
-}
-
-thread_local! {
-  static KEYWORD_TOKENS: KeywordTokens = keyword_tokens();
-}
-
-#[derive(Clone, Debug, PartialEq, Eq, Hash, Default)]
-struct Identifier {
-    value: String,
-}
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Default)]
-struct IntLiteral {
-    value: u128,
-}
-
-struct Lexer {
-    chars: Vec<char>,
-    index: usize,
-}
-
-impl Lexer {
-    pub fn lex(str: &str) -> Vec<Tok> {
-        let mut out = vec![];
-        let mut lexer = Lexer::new(str);
-        loop {
-            let tok_kind = lexer.next();
-            if tok_kind == TokKind::EndOfInput {
-                return out;
+    impl SourceInfo {
+        pub fn span(&self, other: SourceInfo) -> SourceInfo {
+            let end = other.start + other.length;
+            SourceInfo {
+                start: self.start,
+                length: end - self.start,
             }
-            out.push(Tok::new(tok_kind));
         }
     }
-    fn new(str: &str) -> Self {
-        Lexer {
-            chars: str.chars().collect(),
-            index: 0,
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn span() {
+            let left = SourceInfo {
+                start: 0,
+                length: 1,
+            };
+            let right = SourceInfo {
+                start: 10,
+                length: 2,
+            };
+            assert_eq!(
+                left.span(right),
+                SourceInfo {
+                    start: 0,
+                    length: 12
+                }
+            )
         }
     }
-    fn peek(&self) -> char {
-        if self.index >= self.chars.len() {
-            return '\0';
+}
+
+mod token {
+    use crate::source_info::SourceInfo;
+    use std::{collections::HashMap, rc::Rc};
+
+    #[derive(Clone, Debug, PartialEq, Eq)]
+    pub struct Tok {
+        pub kind: TokKind,
+        pub source_info: SourceInfo,
+    }
+
+    #[derive(Clone, Debug, PartialEq, Eq, Hash)]
+    pub enum TokKind {
+        // keywords
+        Let,
+        While,
+        Do,
+        End,
+        Return,
+        Func,
+        Struct,
+        If,
+        Then,
+        Else,
+        As,
+        True,
+        False,
+        // punctuation
+        Colon,
+        ColonEq,
+        ParLeft,
+        ParRight,
+        Arrow,
+        Semi,
+        AngleLeft,
+        AngleRight,
+        And,
+        Dot,
+        Comma,
+        Plus,
+        Minus,
+        Star,
+        Slash,
+        At,
+        Identifier(Identifier),
+        IntLiteral(IntLiteral),
+        // control
+        Ignored,
+        EndOfInput,
+        Invalid,
+    }
+
+    impl TokKind {
+        pub fn from_ident(str: String) -> TokKind {
+            KEYWORD_TOKENS
+                .with(|pair| pair.clone())
+                .0
+                .get(&str)
+                .cloned()
+                .unwrap_or(TokKind::Identifier(Identifier { value: str }))
         }
-        self.chars[self.index]
-    }
-    fn advance(&mut self) {
-        if self.index == self.chars.len() {
-            panic!("unexpected end of input")
-        }
-        self.index += 1;
-    }
-    fn adv_next(&mut self, tok: TokKind) -> TokKind {
-        self.advance();
-        tok
-    }
-    fn next(&mut self) -> TokKind {
-        match self.peek() {
-            '\0' => TokKind::EndOfInput,
-            '#' => self.comment(),
-            ' ' | '\t' | '\n' => self.whitespace(),
-            '0'..='9' => self.number(),
-            'a'..='z' | 'A'..='Z' => self.identifier_or_keyword(),
-            '+' => self.adv_next(TokKind::Plus),
-            '*' => self.adv_next(TokKind::Star),
-            '(' => self.adv_next(TokKind::ParLeft),
-            ')' => self.adv_next(TokKind::ParRight),
-            _ => panic!("unknown char"),
+
+        fn to_keyword(&self) -> Option<String> {
+            KEYWORD_TOKENS
+                .with(|pair| pair.clone())
+                .1
+                .get(self)
+                .map(|s| s.to_string())
         }
     }
-    fn comment(&mut self) -> TokKind {
-        loop {
+
+    type KeywordTokens = Rc<(HashMap<String, TokKind>, HashMap<TokKind, String>)>;
+
+    fn keyword_tokens() -> KeywordTokens {
+        let pairs = vec![
+            (TokKind::Let, "let"),
+            (TokKind::While, "while"),
+            (TokKind::Func, "func"),
+            (TokKind::Struct, "struct"),
+            (TokKind::Do, "do"),
+            (TokKind::Return, "return"),
+            (TokKind::If, "if"),
+            (TokKind::Then, "then"),
+            (TokKind::Else, "else"),
+            (TokKind::End, "end"),
+            (TokKind::True, "true"),
+            (TokKind::False, "false"),
+        ];
+
+        Rc::new((
+            pairs
+                .iter()
+                .map(|(token, str)| (str.to_string(), token.clone()))
+                .collect(),
+            pairs
+                .iter()
+                .map(|(token, str)| (token.clone(), str.to_string()))
+                .collect(),
+        ))
+    }
+
+    thread_local! {
+      static KEYWORD_TOKENS: KeywordTokens = keyword_tokens();
+    }
+
+    #[derive(Clone, Debug, PartialEq, Eq, Hash, Default)]
+    pub struct Identifier {
+        value: String,
+    }
+
+    #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash, Default)]
+    pub struct IntLiteral {
+        pub value: u128,
+    }
+}
+
+mod lexer {
+    use crate::source_info::SourceInfo;
+    use crate::token::{IntLiteral, Tok, TokKind};
+
+    pub struct Lexer {
+        chars: Vec<char>,
+        index: usize,
+    }
+
+    impl Lexer {
+        pub fn lex(str: &str) -> Vec<Tok> {
+            let mut out = vec![];
+            let mut lexer = Lexer::new(str);
+            loop {
+                let start = lexer.index;
+                let tok_kind = lexer.next();
+                if tok_kind == TokKind::Ignored {
+                    continue;
+                }
+                if tok_kind == TokKind::EndOfInput {
+                    return out;
+                }
+
+                let length = lexer.index - start;
+                out.push(Tok {
+                    kind: tok_kind,
+                    source_info: SourceInfo { start, length },
+                });
+            }
+        }
+        fn new(str: &str) -> Self {
+            Lexer {
+                chars: str.chars().collect(),
+                index: 0,
+            }
+        }
+        fn peek(&self) -> char {
+            if self.index >= self.chars.len() {
+                return '\0';
+            }
+            self.chars[self.index]
+        }
+        fn advance(&mut self) {
+            if self.index == self.chars.len() {
+                panic!("unexpected end of input")
+            }
+            self.index += 1;
+        }
+        fn adv_next(&mut self, tok: TokKind) -> TokKind {
+            self.advance();
+            tok
+        }
+        fn next(&mut self) -> TokKind {
             match self.peek() {
-                '\n' | '\0' => return self.next(),
-                _ => {
+                '\0' => TokKind::EndOfInput,
+                '#' => self.comment(),
+                ' ' | '\t' | '\n' => self.whitespace(),
+                '0'..='9' => self.number(),
+                'a'..='z' | 'A'..='Z' => self.identifier_or_keyword(),
+                '+' => self.adv_next(TokKind::Plus),
+                '*' => self.adv_next(TokKind::Star),
+                '(' => self.adv_next(TokKind::ParLeft),
+                ')' => self.adv_next(TokKind::ParRight),
+                _ => self.adv_next(TokKind::Invalid),
+            }
+        }
+        fn comment(&mut self) -> TokKind {
+            loop {
+                match self.peek() {
+                    '\n' | '\0' => return TokKind::Ignored,
+                    _ => {
+                        self.advance();
+                    }
+                }
+            }
+        }
+        fn whitespace(&mut self) -> TokKind {
+            loop {
+                if self.peek().is_whitespace() {
+                    self.advance()
+                } else {
+                    return TokKind::Ignored;
+                }
+            }
+        }
+        fn number(&mut self) -> TokKind {
+            let mut sum = 0;
+            while let Some(digit) = self.peek().to_digit(10) {
+                self.advance();
+                sum = sum * 10 + (digit as u128)
+            }
+            TokKind::IntLiteral(IntLiteral { value: sum })
+        }
+        fn identifier_or_keyword(&mut self) -> TokKind {
+            let mut str = String::new();
+            loop {
+                let ch = self.peek();
+                if ch.is_alphanumeric() || ch == '_' {
                     self.advance();
+                    str.push(ch);
+                } else {
+                    return TokKind::from_ident(str);
                 }
             }
         }
     }
-    fn whitespace(&mut self) -> TokKind {
-        loop {
-            if self.peek().is_whitespace() {
-                self.advance()
-            } else {
-                return self.next();
-            }
+
+    #[cfg(test)]
+    mod test {
+        use super::*;
+
+        #[test]
+        fn empty_program() {
+            assert_eq!(Lexer::lex(""), vec![])
         }
-    }
-    fn number(&mut self) -> TokKind {
-        let mut sum = 0;
-        while let Some(digit) = self.peek().to_digit(10) {
-            self.advance();
-            sum = sum * 10 + (digit as u128)
+
+        #[test]
+        fn token_with_source_info() {
+            assert_eq!(
+                Lexer::lex("  123  "),
+                vec![Tok {
+                    kind: TokKind::IntLiteral(IntLiteral { value: 123 }),
+                    source_info: SourceInfo {
+                        start: 2,
+                        length: 3
+                    }
+                }]
+            )
         }
-        TokKind::IntLiteral(IntLiteral { value: sum })
-    }
-    fn identifier_or_keyword(&mut self) -> TokKind {
-        let mut str = String::new();
-        loop {
-            let ch = self.peek();
-            if ch.is_alphanumeric() || ch == '_' {
-                self.advance();
-                str.push(ch);
-            } else {
-                return TokKind::from_ident(str);
-            }
+
+        #[test]
+        fn invalid_token() {
+            assert_eq!(
+                Lexer::lex(" \x01 "),
+                vec![Tok {
+                    kind: TokKind::Invalid,
+                    source_info: SourceInfo {
+                        start: 1,
+                        length: 1
+                    }
+                }]
+            )
         }
     }
 }
 
+use crate::lexer::Lexer;
+use crate::source_info::SourceInfo;
+use crate::token::{IntLiteral, Tok, TokKind};
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 struct Expr {
     kind: ExprKind,
+    source_info: SourceInfo,
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -273,17 +371,20 @@ struct Parser {
 #[derive(Clone, Debug, PartialEq, Eq)]
 struct ParseError {
     kind: ParseErrKind,
+    source_info: SourceInfo,
 }
 
 impl ParseError {
-    fn expected_token(tok: TokKind) -> Self {
+    fn expected_token(tok: TokKind, source_info: SourceInfo) -> Self {
         Self {
             kind: ParseErrKind::ExpectedToken(tok),
+            source_info,
         }
     }
-    fn expected(name: &str) -> Self {
+    fn expected(name: &str, source_info: SourceInfo) -> Self {
         Self {
             kind: ParseErrKind::Expected(String::from(name)),
+            source_info,
         }
     }
 }
@@ -297,17 +398,10 @@ enum ParseErrKind {
 type Parse<T> = Result<T, ParseError>;
 type ParseOpt<T> = Result<Option<T>, ParseError>;
 
-fn expect<T>(name: &str, value: ParseOpt<T>) -> Parse<T> {
-    match value {
-        Ok(Some(value)) => Ok(value),
-        _ => Err(ParseError::expected(name)),
-    }
-}
-
 impl Parser {
     pub fn parse_expr(tokens: Vec<Tok>) -> Parse<Expr> {
         let mut parser = Self::new(tokens);
-        expect("expr", parser.expr())
+        parser.expect_p("expr", Self::expr)
     }
 
     fn new(tokens: Vec<Tok>) -> Self {
@@ -320,8 +414,27 @@ impl Parser {
             .unwrap_or(TokKind::EndOfInput)
     }
 
+    fn peek_source(&self) -> SourceInfo {
+        self.tokens
+            .get(self.index)
+            .map(|t| t.source_info)
+            .unwrap_or(SourceInfo {
+                start: self.index,
+                length: 0,
+            })
+    }
+
     fn advance(&mut self) {
         self.index += 1
+    }
+
+    fn expect_p<T>(&mut self, name: &str, get_value: fn(&mut Self) -> ParseOpt<T>) -> Parse<T> {
+        let val = get_value(self);
+        match val {
+            Ok(Some(value)) => Ok(value),
+            Ok(None) => Err(ParseError::expected(name, self.peek_source())),
+            Err(e) => Err(e),
+        }
     }
 
     fn expect_token(&mut self, tok: TokKind) -> Parse<()> {
@@ -329,7 +442,7 @@ impl Parser {
             self.advance();
             Ok(())
         } else {
-            Err(ParseError::expected_token(tok))
+            Err(ParseError::expected_token(tok, self.peek_source()))
         }
     }
 
@@ -341,13 +454,15 @@ impl Parser {
         if let Some(mut left) = parse_operand(self)? {
             loop {
                 if let Some(operator) = parse_operator(self)? {
-                    let right = expect("expr", parse_operand(self))?;
+                    let right = self.expect_p("expr", parse_operand)?;
+                    let source_info = left.source_info.span(right.source_info);
                     left = Expr {
                         kind: ExprKind::BinaryOp(Box::new(BinaryOp {
                             operator,
                             left,
                             right,
                         })),
+                        source_info,
                     };
                 } else {
                     return Ok(Some(left));
@@ -385,14 +500,16 @@ impl Parser {
         match self.peek() {
             TokKind::ParLeft => {
                 self.advance();
-                let expr = expect("expr", self.expr())?;
+                let expr = self.expect_p("expr", Self::expr)?;
                 self.expect_token(TokKind::ParRight)?;
                 Ok(Some(expr))
             }
             TokKind::IntLiteral(payload) => {
+                let source_info = self.peek_source();
                 self.advance();
                 Ok(Some(Expr {
                     kind: ExprKind::IntLiteral(payload),
+                    source_info,
                 }))
             }
             _ => Ok(None),
@@ -439,11 +556,6 @@ mod test {
             .eval_expr(Parser::parse_expr(Lexer::lex(str)).expect("expr"))
             .expect("value");
         assert_eq!(result, expected);
-    }
-
-    #[test]
-    fn empty_program() {
-        assert_eq!(Lexer::lex(""), vec![])
     }
 
     #[test]
